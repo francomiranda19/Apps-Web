a7c84dc3f390b41fc48a7cbdd4b58c2a3f84bdb5
Franco Miranda <francomiranda1901@gmail.com>
2021-04-04 02:23:28 -0400

    Creado javascript

---
+++ Tarea 1/scripts/informar-avistamiento.js
@@ -0,0 +1,26 @@
+let r = [
+    "Región de Arica y Parinacota",
+    "Región de Tarapacá",
+    "Región de Antofagasta",
+    "Región de Atacama",
+    "Región de Coquimbo",
+    "Región de Valparaíso",
+    "Región Metropolitana de Santiago",
+    "Región del Libertador General Bernardo O'Higgins",
+    "Región del Maule",
+    "Región del Ñuble",
+    "Región del Biobío",
+    "Región de La Araucanía",
+    "Región de Los Ríos",
+    "Región de Los Lagos",
+    "Región de Aysén del General Carlos Ibáñez del Campo",
+    "Región de Magallanes y la Antártica Chilena",
+]
+
+for (i in r) {
+    document.write("<option>" + r[i] + "</option>");
+}
+
+function comunas() {
+    
+}
