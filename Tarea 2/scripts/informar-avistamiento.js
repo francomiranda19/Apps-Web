85029088185a8bb57441e492d8514a8bc11b2f94
Franco Miranda <francomiranda1901@gmail.com>
2021-04-27 01:07:00 -0400

    Agregados archivos necesarios para la tarea 2

---
+++ Tarea 2/scripts/informar-avistamiento.js
@@ -0,0 +1,766 @@
+let comunas_arica_y_parinacota = [
+    "Arica",
+    "Camarones",
+    "General Lagos",
+    "Putre"
+]
+
+let comunas_tarapaca = [
+    "Alto Hospicio",
+    "Camiña",
+    "Colchane",
+    "Iquique"
+]
+
+let comunas_antofagasta = [
+    "Antofagasta",
+    "Calama",
+    "María Elena",
+    "Mejillones",
+    "Ollagüe",
+    "San Pedro de Atacama",
+    "Sierra Gorda",
+    "Taltal",
+    "Tocopilla"
+]
+
+let comunas_atacama = [
+    "Alto del Carmen",
+    "Caldera",
+    "Chañaral",
+    "Copiapó",
+    "Diego de Almagro",
+    "Freirina",
+    "Huasco",
+    "Tierra Amarilla",
+    "Vallenar"
+]
+
+let comunas_coquimbo = [
+    "Andacollo",
+    "Canela",
+    "Combarbalá",
+    "Coquimbo",
+    "Illapel",
+    "La Higuera",
+    "La Serena",
+    "Los Vilos",
+    "Monte Patria",
+    "Ovalle",
+    "Paihuano",
+    "Punitaqui",
+    "Río Hurtado",
+    "Salamanca",
+    "Vicuña"
+]
+
+let comunas_valparaiso = [
+    "Calle Larga",
+    "Isla de Pascua",
+    "Limache",
+    "Los Andes",
+    "Olmué",
+    "Quilpué",
+    "Rinconada",
+    "San Esteban",
+    "Villa Alemana"
+]
+
+let comunas_metropolitana = [
+    "Alhué",
+    "Buin",
+    "Calera de Tango",
+    "Cerrillos",
+    "Cerro Navia",
+    "Colina",
+    "Conchalí",
+    "Curacaví",
+    "El Bosque",
+    "El Monte",
+    "Estación Central",
+    "Huechuraba",
+    "Independencia",
+    "Isla de Maipo",
+    "Lampa",
+    "La Cisterna",
+    "La Granja",
+    "La Florida",
+    "La Pintana",
+    "La Reina",
+    "Las Condes",
+    "Lo Barnechea",
+    "Lo Espejo",
+    "Lo Prado",
+    "Macul",
+    "Maipú",
+    "María Pinto",
+    "Melipilla",
+    "Ñuñoa",
+    "Padre Hurtado",
+    "Paine",
+    "Pedro Aguirre Cerda",
+    "Peñaflor",
+    "Peñalolén",
+    "Pirque",
+    "Providencia",
+    "Pudahuel",
+    "Puente Alto",
+    "Quilicura",
+    "Quinta Normal",
+    "Recoleta",
+    "Renca",
+    "San Bernardo",
+    "San Joaquín",
+    "San José de Maipo",
+    "San Miguel",
+    "San Pedro",
+    "San Ramón",
+    "Santiago",
+    "Talagante",
+    "Tiltil",
+    "Vitacura"
+]
+
+let comunas_ohiggins = [
+    "Chépica",
+    "Chimbarongo",
+    "Codegua",
+    "Coinco",
+    "Coltauco",
+    "Doñihue",
+    "Graneros",
+    "La Estrella",
+    "Las Cabras",
+    "Litueche",
+    "Lolol",
+    "Machalí",
+    "Malloa",
+    "Marchigüe",
+    "Mostazal",
+    "Nancagua",
+    "Navidad",
+    "Palmilla",
+    "Paredones",
+    "Peralillo",
+    "Pichilemu",
+    "Placilla",
+    "Pumanque",
+    "Olivar",
+    "Peumo",
+    "Pichidegua",
+    "Quinta de Tilcoco",
+    "Rancagua",
+    "Rengo",
+    "Requinoa",
+    "San Fernando",
+    "San Vicente de Tagua Tagua",
+    "Santa Cruz"
+]
+
+let comunas_maule = [
+    "Cauquenes",
+    "Chanco",
+    "Colbún",
+    "Constitución",
+    "Curepto",
+    "Curicó",
+    "Empedrado",
+    "Hualañe",
+    "Licantén",
+    "Linares",
+    "Longaví",
+    "Maule",
+    "Molina",
+    "Parral",
+    "Pelarco",
+    "Pelluhue",
+    "Pencahue",
+    "Rauco",
+    "Retiro",
+    "Río Claro",
+    "Romeral",
+    "Sagrada Familia",
+    "San Clemente",
+    "San Javier",
+    "San Rafael",
+    "Talca",
+    "Teno",
+    "Vichuquén",
+    "Villa Alegre",
+    "Yerbas Buenas"
+]
+
+let comunas_nuble = [
+    "Bulnes",
+    "Chillán Viejo",
+    "Chillán",
+    "Cobquecura",
+    "Coelemu",
+    "Coihueco",
+    "El Carmén",
+    "Ninhue",
+    "Ñiquén",
+    "Pemuco",
+    "Pinto",
+    "Portezuelo",
+    "Quillón",
+    "Quirihue",
+    "Ránquil",
+    "San Carlos",
+    "San Fabián",
+    "San Ignacio",
+    "San Nicolás",
+    "Trehuaco",
+    "Yungay"
+]
+
+let comunas_biobio = [
+    "Alto Biobío",
+    "Antuco",
+    "Arauco",
+    "Cabrero",
+    "Cañete",
+    "Contulmo",
+    "Curanilahue",
+    "Laja",
+    "Lebu",
+    "Los Álamos",
+    "Los Ángeles",
+    "Mulchén",
+    "Nacimiento",
+    "Negrete",
+    "Quilaco",
+    "Quilleco",
+    "San Rosendo",
+    "Santa Bárbara",
+    "Tirúa",
+    "Tucapel",
+    "Yumbel"
+]
+
+let comunas_araucania = [
+    "Angol",
+    "Carahue",
+    "Cholchol",
+    "Collipulli",
+    "Cunco",
+    "Curacautín",
+    "Curarrehue",
+    "Ercilla",
+    "Freire",
+    "Galvarino",
+    "Gorbea",
+    "Lautaro",
+    "Loncoche",
+    "Lonquimay",
+    "Los Sauces",
+    "Lumaco",
+    "Melipeuco",
+    "Nueva Imperial",
+    "Padre Las Casas",
+    "Perquenco",
+    "Pitrufquén",
+    "Pucón",
+    "Purén",
+    "Puerto Saavedra",
+    "Renaico",
+    "Temuco",
+    "Teodoro Schmidt",
+    "Toltén",
+    "Traiguén",
+    "Victoria",
+    "Vilcún",
+    "Vilarrica"
+]
+
+let comunas_rios = [
+    "Corral",
+    "Futrono",
+    "Lanco",
+    "La Unión",
+    "Lago Ranco",
+    "Los Lagos",
+    "Máfil",
+    "Mariquina",
+    "Paillaco",
+    "Panguipulli",
+    "Río Bueno",
+    "Valdivia"
+]
+
+let comunas_lagos = [
+    "Ancud",
+    "Calbuco",
+    "Castro",
+    "Chaitén",
+    "Chonchi",
+    "Cochamó",
+    "Curaco de Vélez",
+    "Dalcahue",
+    "Fresia",
+    "Frutillar",
+    "Futaleufú",
+    "Hualaihué",
+    "Llanquihue",
+    "Los Muermos",
+    "Maullín",
+    "Osorno",
+    "Palena",
+    "Puerto Montt",
+    "Puerto Octay",
+    "Puerto Varas",
+    "Puqueldón",
+    "Purranque",
+    "Puyehue",
+    "Queilén",
+    "Quemchi",
+    "Quellón",
+    "Quinchao",
+    "Río Negro",
+    "San Juan de la Costa",
+    "San Pablo"
+]
+
+let comunas_aysen = [
+    "Aysén",
+    "Chile Chico",
+    "Cisnes",
+    "Cochrane",
+    "Coyhaique",
+    "Guaitecas",
+    "Lago Verde",
+    "O´Higgins",
+    "Río Ibáñez",
+    "Tortel"
+]
+
+let comunas_magallanes = [
+    "Antártica",
+    "Cabo de Hornos",
+    "Laguna Blanca",
+    "Natales",
+    "Porvenir",
+    "Primavera",
+    "Punta Arenas",
+    "Río Verde",
+    "San Gregorio",
+    "Timaukel",
+    "Torres del Paine"
+]
+
+let formularios = 1;
+let imagenes = 1;
+
+function calcular_comunas() {
+    let region = document.getElementsByName("region")[0].selectedIndex;
+    let comunas = document.getElementsByName("comuna")[0];
+
+    // Se eliminan comunas pertenecientes a otras regiones
+    for (i in comunas) {
+        comunas.options.remove(i);
+    }
+
+    comunas.options.add(new Option("Elija una opción"));
+    // Se muestran comunas según su región
+    if (region == 1) {
+        for (i in comunas_arica_y_parinacota) {
+            let option = new Option(comunas_arica_y_parinacota[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 2) {
+        for (i in comunas_tarapaca) {
+            let option = new Option(comunas_tarapaca[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 3) {
+        for (i in comunas_antofagasta) {
+            let option = new Option(comunas_antofagasta[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 4) {
+        for (i in comunas_atacama) {
+            let option = new Option(comunas_atacama[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 5) {
+        for (i in comunas_coquimbo) {
+            let option = new Option(comunas_coquimbo[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 6) {
+        for (i in comunas_valparaiso) {
+            let option = new Option(comunas_valparaiso[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 7) {
+        for (i in comunas_metropolitana) {
+            let option = new Option(comunas_metropolitana[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 8) {
+        for (i in comunas_ohiggins) {
+            let option = new Option(comunas_ohiggins[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 9) {
+        for (i in comunas_maule) {
+            let option = new Option(comunas_maule[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 10) {
+        for (i in comunas_nuble) {
+            let option = new Option(comunas_nuble[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 11) {
+        for (i in comunas_biobio) {
+            let option = new Option(comunas_biobio[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 12) {
+        for (i in comunas_araucania) {
+            let option = new Option(comunas_araucania[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 13) {
+        for (i in comunas_rios) {
+            let option = new Option(comunas_rios[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 14) {
+        for (i in comunas_lagos) {
+            let option = new Option(comunas_lagos[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 15) {
+        for (i in comunas_aysen) {
+            let option = new Option(comunas_aysen[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    } if (region == 16) {
+        for (i in comunas_magallanes) {
+            let option = new Option(comunas_magallanes[i]);
+            option.setAttribute("value", i);
+            comunas.options.add(option);
+        }
+    }
+}
+
+function agregar_imagen() {
+    // Se aumenta la cantidad de imagenes y se crea el nuevo input
+    imagenes++;
+
+    let input = document.getElementsByClassName("imagenes")[formularios - 1].childNodes;
+    let newInput = input[1].cloneNode();
+    let fotos = document.getElementsByClassName("imagenes")[formularios - 1];
+    let boton = document.getElementsByClassName("boton-agregar")[formularios - 1];
+
+    // Se setea nuevo id para el nuevo input
+    // Se setea un nuevo input sin ningún archivo
+    newInput.value = "";
+    fotos.appendChild(newInput);
+
+    // Si ya se agregaron 5 fotos, el botón desaparece
+    if (imagenes == 5) {
+        boton.style.visibility = "hidden";
+    }
+}
+
+function crear_nuevo_formulario() {
+    // El nuevo formulario se crea solo si pasa la validación
+    if (validar(false)) {
+        let boton = document.getElementsByClassName("boton-agregar")[formularios - 1];
+        boton.style.visibility = "visible";
+        let info = document.getElementsByClassName("info-avistamiento")[formularios - 1];
+        let newInfo = info.cloneNode(true);
+    
+        //document.write(nuevasFotos);
+        boton.style.visibility = "hidden";
+
+        // Se deshabilita la información de avistamiento del formulario anterior
+        document.getElementsByName("dia-hora-avistamiento")[formularios - 1].disabled = true;
+        document.getElementsByName("tipo-avistamiento")[formularios - 1].disabled = true;
+        document.getElementsByName("estado-avistamiento")[formularios - 1].disabled = true;
+        let fotos = document.getElementsByName("foto-avistamiento");
+        for (foto of fotos) {
+            foto.disabled = true;
+        }
+        
+        // Se aumenta la cantidad de formularios totales
+        formularios++;
+        
+        // Se inserta el nuevo formulario al final del otro formulario
+        info.insertAdjacentElement("afterend", newInfo);
+        
+        // Se limpia el nuevo formulario
+        document.getElementsByName("dia-hora-avistamiento")[formularios - 1].value = "";
+        document.getElementsByName("tipo-avistamiento")[formularios - 1].selectedIndex = 0;
+        document.getElementsByName("estado-avistamiento")[formularios - 1].selectedIndex = 0;
+        
+        let newFotos = document.getElementsByClassName("imagenes")[formularios - 1].childNodes;
+        newFotos[1].value = "";
+        while (newFotos.length > 2) {
+            newFotos[2].remove();
+        }
+
+        // La cantidad de imagenes vuelve a ser la inicial
+        imagenes = 1;
+    }
+}
+
+function cerrar_ventana_emergente() {
+    let ventana = document.getElementsByClassName("ventana-emergente")[0];
+
+    // En caso de apretar la opción "No..." se regresa al formulario
+    ventana.style.visibility = "hidden";
+}
+
+/*------------------------------ VALIDACIONES -------------------------------*/
+let errores = "";
+function validar_region() {
+    let i = document.getElementsByName("region")[0].selectedIndex;
+
+    // Se verifica que se haya seleccionado una región
+    if (i == 0) {
+        errores += "Debe elegir una región\n"
+        return false
+    } else {
+        return true;
+    }
+}
+
+function validar_comuna() {
+    let i = document.getElementsByName("comuna")[0].selectedIndex;
+
+    // Se verifica que se haya seleccionado una comuna
+    if (i == 0) {
+        errores += "Debe elegir una comuna\n";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validar_sector() {
+    let sec = document.getElementsByName("sector")[0].value;
+    let secRegex = /^[0-9a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[0-9a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[0-9a-zA-ZÀ-ÿ\u00f1\u00d1]+$/;
+
+    // Si no se ingresó sector, está correcto
+    if (sec.length == 0) {
+        return true;
+    }
+    // Si el sector supera los 100 caracteres hay un error
+    if (sec.length > 100) {
+        errores += "Nombre del sector demasiado largo\n";
+        return false;
+    }
+
+    // Se verifica que el sector solo posea caracteres alfa numéricos
+    if (sec.match(secRegex)) {
+        return true;
+    } else {
+        errores += "Nombre del sector inválido\n";
+        return false;
+    }
+}
+
+function validar_nombre() {
+    let name = document.getElementsByName("nombre")[0].value;
+    let nameRegex = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/;
+
+    // Si no se ingresa nombre, hay un error
+    if (name.length == 0) {
+        errores += "Debe ingresar nombre\n";
+        return false;
+    }
+    // Si el nombre tiene más de 200 caracteres hay un error
+    if (name.length > 200) {
+        errores += "Nombre demasiado largo\n";
+        return false;
+    }
+
+    // Se verifica que el nombre solo contenga letras
+    if (name.match(nameRegex)) {
+        return true;
+    } else {
+        errores += "Nombre inválido\n";
+        return false;
+    }
+}
+
+function validar_email() {
+    let correo = document.getElementsByName("email")[0].value;
+    let correoRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+    // Si no se ingresó correo hay un error
+    if (correo.length == 0) {
+        errores += "Debe ingresar email\n";
+        return false;
+    }
+
+    // Si el email tiene más de 100 caracteres hay un error
+    if (correo.length > 100) {
+        errores += "Email demasiado largo\n";
+        return false;
+    }
+    // Se testea que se cumpla el formato de email
+    if (correo.match(correoRegex)) {
+        return true;
+    } else {
+        errores += "Email incorrecto, no cumple formato\n";
+        return false;
+    }
+}
+
+function validar_celular() {
+    let cel = document.getElementsByName("celular")[0].value;
+    let celRegex = /^[+][0-9]+$/;
+
+    // Si no hay número de celular, se acepta
+    if (cel.length == 0) {
+        return true;
+    }
+
+    // Si el número de celular tiene largo mayor a 15, hay error
+    if (cel.length > 15) {
+        errores += "Número de celular demasiado largo\n";
+        return false;
+    }
+
+    // Se testea que se solo hayan números y que el primer carácter sea un +
+    if (cel.match(celRegex)) {
+        return true;
+    } else {
+        errores += "Número de celular inválido\n"
+        return false;
+    }
+}
+
+function validar_dia_hora() {
+    let fecha = document.getElementsByName("dia-hora-avistamiento")[formularios - 1].value;
+    let fechaRegex = /^\d{2,4}\-\d{1,2}\-\d{1,2}[\s]([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+    
+    // Si no se ingresó fecha, hay error
+    if (fecha.length == 0) {
+        errores += "Debe ingresar día y hora\n";
+        return false;
+    }
+    // Si la fecha ingresada es demasiado larga, hay error
+    if (fecha.length > 20) {
+        errores += "Día y hora ingresados son demasiado largos\n";
+        return false;
+    }
+
+    // Se verifica que se cumpla el formato pedido
+    if (fecha.match(fechaRegex)) {
+        return true;
+    } else {
+        errores += "Día y hora no cumple el formato solicitado\n";
+        return false;
+    }
+}
+
+function validar_tipo() {
+    let i = document.getElementsByName("tipo-avistamiento")[formularios - 1].selectedIndex;
+    // Se verifica que se haya seleccionado una comuna
+    if (i == 0) {
+        errores += "Debe elegir un tipo\n";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validar_estado() {
+    let i = document.getElementsByName("estado-avistamiento")[formularios - 1].selectedIndex;
+    // Se verifica que se haya seleccionado una comuna
+    if (i == 0) {
+        errores += "Debe elegir un estado\n";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validar_foto() {
+    let cantidad = document.getElementsByClassName("imagenes")[formularios - 1].childNodes[1].files.length;
+    //let cantidad = document.getElementsByName("foto-avistamiento")[formularios - 1].files.length;
+
+    // Si no se ha agregado ninguna foto, hay un error
+    if (cantidad == 0) {
+        errores += "Debe enviar al menos una foto\n";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validar_extension_imagen(node) {
+    let fileValue = node.value;
+    let extensiones = /(.jpg|.jpeg|.png|.gif)$/i;
+    // Se chequea si el archivo ingresado efectivamente es una imagen
+    if (!extensiones.exec(fileValue)) {
+        alert("El archivo ingresado no tiene un formato de imágen válido");
+        node.value = "";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validar(activar_ventana) {
+    // Se realizan todas las validaciones necesarias para enviar la información
+    let validaciones = [
+        validar_region(),
+        validar_comuna(),
+        validar_sector(),
+        validar_nombre(),
+        validar_email(),
+        validar_celular(),
+        validar_dia_hora(),
+        validar_tipo(),
+        validar_estado(),
+        validar_foto()
+    ];
+
+    const valueIsTrue = (element) => element == true;
+
+    // Si se quiere enviar la información
+    if (activar_ventana) {
+        // Si se pasan todas las validaciones, se muestra la ventana emergente
+        if (validaciones.every(valueIsTrue)) {
+            document.getElementsByClassName("ventana-emergente")[0].style.visibility = "visible";
+            return true;
+        } else {
+            alert(errores);
+            errores = "";
+            return false;
+        }
+    }
+    // Si se quiere informar otro avistamiento
+    else {
+        // Si se pasan las validaciones necesarias, se crea el nuevo formulario
+        if (validaciones.every(valueIsTrue)) {
+            return true;
+        } else {
+            alert(errores);
+            errores = "";
+            return false;
+        }
+    }
+}
