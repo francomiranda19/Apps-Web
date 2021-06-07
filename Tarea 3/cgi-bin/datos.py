e56dfba4ce9811a399da4cdd8b80f9ff37499b16
Franco Miranda <francomiranda1901@gmail.com>
2021-06-07 03:46:22 -0400

    Gráficos implementados

--- Tarea 3/cgi-bin/datos.py
+++ Tarea 3/cgi-bin/datos.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import cgi
 import json
-
 from db import Avistamiento
 
 utf8stdout = open(1, "w", encoding="utf-8", closefd=False)
@@ -15,25 +13,29 @@ conteo_vivos = bbdd.get_conteo_estados("vivo")
 conteo_muertos = bbdd.get_conteo_estados("muerto")
 conteo_nose = bbdd.get_conteo_estados("no sé")
 
-puntos = {"x1": [1, 2, 3, 4, 5, 6, 7], "y1": [0, 0, 0, 0, 0, 0, 0],
+puntos = {"y1": [0, 0, 0, 0, 0, 0, 0],
           "tipo": ["arácnido", "insecto", "miriápodo", "no sé"], "y2": [0, 0, 0, 0],
-          "x3": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], "vivo": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          "muerto": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "no se": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+          "vivo": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "muerto": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          "no se": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
 
 for fila in avistamientos_por_dia:
-    puntos["y1"][fila[0] - 2] = fila[1]
+    i = fila[0] - 2
+    puntos["y1"][i] = fila[1]
 
 for fila in conteo_tipos:
     i = puntos["tipo"].index(fila[0])
     puntos["y2"][i] = fila[1]
 
 for fila in conteo_vivos:
-    puntos["vivo"][fila[0] - 1] = fila[1]
+    i = fila[0] - 1
+    puntos["vivo"][i] = fila[1]
 
 for fila in conteo_muertos:
-    puntos["muerto"][fila[0] - 1] = fila[1]
+    i = fila[0] - 1
+    puntos["muerto"][i] = fila[1]
 
 for fila in conteo_nose:
-    puntos["no se"][fila[0] - 1] = fila[1]
+    i = fila[0] - 1
+    puntos["no se"][i] = fila[1]
 
 print(json.dumps(puntos), file=utf8stdout)
--- Tarea 3/scripts/graficos.js
+++ Tarea 3/scripts/graficos.js
@@ -1,40 +1,64 @@
-$(function($) {
-    $("#grafico-1").highcharts({
-        title: {text: "<b>Cantidad de avistamientos informados por día</b>"},
-        xAxis: {categories: ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"], title: {text: "<b>Día</b>"}},
-        yAxis: {plotLines: [{value: 0, width: 1, color: "#808080"}], title: {text: "<b>Cantidad</b>"}},
-        legend: {layout: "vertical", align: "right", verticalAlign: "middle", borderWidth: 0},
-        series: [{name: "Cantidad de avistamientos", color: "#e84118", data: [25, 23, 21, 20, 22, 25, 23]}],
-        plotOptions: {line: {dataLabels: {enabled: true}}}
-    });
+function graficos() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "../cgi-bin/datos.py");
+    xhr.onload = (data) => {
+        let json = JSON.parse(data.currentTarget.responseText);
 
-    $("#grafico-2").highcharts({
-        chart: {type: "pie", plotBackGroundColor: "#f8f9fa", plotShadow: false},
-        title: {text: "<b>Cantidad y porcentaje de avistamientos informados según tipo</b>"},
-        tooltip: {pointFormat: "<b>{point.y} ({point.percentage: .2f}%)</b>"},
-        plotOptions: {pie: {allowPointSelect: true, cursor: "pointer", dataLabels: {enabled: true, format: "<b>{point.name} </b><br>{point.y} ({point.percentage: .2f}%)"}}},
-        series: [{
-            name: "Tipos",
-            colorByPoint: true,
-            data: [
-                {name: "Arácnido", y: 560, color: "#e84118"},
-                {name: "Insecto", y: 400, color: "#0097e6"},
-                {name: "Miriápodo", y: 300, color: "#44bd32"},
-                {name: "No sé", y: 53, color: "#dcdde1"}
-            ]
-        }]
-    });
+        // Gráfico 1
+        let grafico_1 = {
+            title: {text: "<b>Cantidad de avistamientos informados por día</b>"},
+            xAxis: {categories: ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"], title: {text: "<b>Día</b>"}},
+            yAxis: {plotLines: [{value: 0, width: 1, color: "#808080"}], title: {text: "<b>Cantidad</b>"}},
+            legend: {layout: "vertical", align: "right", verticalAlign: "middle", borderWidth: 0},
+            series: [{name: "Cantidad de avistamientos", color: "#e84118", data: [0, 0, 0, 0, 0, 0, 0]}],
+            plotOptions: {line: {dataLabels: {enabled: true}}}
+        };
+        grafico_1.series[0].data = json["y1"];
+        $("#grafico-1").highcharts(grafico_1);
 
-    $("#grafico-3").highcharts({
-        title: {text: "<b>Cantidad de avistamientos informados por mes según estado</b>"},
-        xAxis: {categories: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"], title: {text: "<b>Mes</b>"}},
-        yAxis: {plotLines: [{value: 0, width: 1, color: "#808080"}], title: {text: "<b>Cantidad</b>"}},
-        legend: {layout: "vertical", align: "right", verticalAlign: "middle", borderWidth: 0},
-        series: [
-            {type: "column", colorByPoint: false, color: "#e84118", name: "Vivo", data: [25, 23, 21, 43, 21, 32, 44, 33, 67, 10, 15, 50]},
-            {type: "column", colorByPoint: false, color: "#0097e6", name: "Muerto", data: [20, 18, 19, 25, 23, 21, 43, 21, 40, 30, 20, 10]},
-            {type: "column", colorByPoint: false, color: "#44bd32", name: "No sé", data: [15, 17, 11, 15, 20, 25, 23, 30, 31, 33, 35, 40]}
-        ],
-        plotOptions: {line: {dataLabels: {enabled: true}}}
-    });
-});
+        // Gráfico 2
+        let grafico_2 = {
+            chart: {type: "pie", plotBackGroundColor: "#f8f9fa", plotShadow: false},
+            title: {text: "<b>Cantidad y porcentaje de avistamientos informados según tipo</b>"},
+            tooltip: {pointFormat: "<b>{point.y} ({point.percentage: .2f}%)</b>"},
+            plotOptions: {pie: {allowPointSelect: true, cursor: "pointer", dataLabels: {enabled: true, format: "<b>{point.name} </b><br>{point.y} ({point.percentage: .2f}%)"}}},
+            series: [{
+                name: "Tipos",
+                colorByPoint: true,
+                data: [
+                    {name: "Arácnido", y: 0, color: "#e84118"},
+                    {name: "Insecto", y: 0, color: "#0097e6"},
+                    {name: "Miriápodo", y: 0, color: "#44bd32"},
+                    {name: "No sé", y: 0, color: "#dcdde1"}
+                ]
+            }]
+        };
+        for (let i in json["y2"]) {
+            grafico_2.series[0].data[i].y = json["y2"][i];
+        }
+        $("#grafico-2").highcharts(grafico_2);
+
+        // Gráfico 3
+        let grafico_3 = {
+            title: {text: "<b>Cantidad de avistamientos informados por mes según estado</b>"},
+            xAxis: {categories: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"], title: {text: "<b>Mes</b>"}},
+            yAxis: {plotLines: [{value: 0, width: 1, color: "#808080"}], title: {text: "<b>Cantidad</b>"}},
+            legend: {layout: "vertical", align: "right", verticalAlign: "middle", borderWidth: 0},
+            series: [
+                {type: "column", colorByPoint: false, color: "#e84118", name: "Vivo", data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+                {type: "column", colorByPoint: false, color: "#0097e6", name: "Muerto", data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+                {type: "column", colorByPoint: false, color: "#44bd32", name: "No sé", data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+            ],
+            plotOptions: {line: {dataLabels: {enabled: true}}}
+        };
+        grafico_3.series[0].data = json["vivo"];
+        grafico_3.series[1].data = json["muerto"];
+        grafico_3.series[2].data = json["no se"];
+        $("#grafico-3").highcharts(grafico_3);
+    }
+    xhr.send();
+}
+
+function init() {
+    graficos();
+}
